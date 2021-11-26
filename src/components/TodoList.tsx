f8fa5a27099377475caa9d3e7fdcc166e0515789
Nelson Foltz <nelsfoltz@gmail.com>
2021-11-26 16:59:41 -0500

    initial commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import './TodoList.css';
+
+interface TodoListProps {
+  items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
+}
+
+const TodoList: FC<TodoListProps> = props => {
+  return (
+    <ul>
+      {props.items.map(todo => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            DELETE
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
